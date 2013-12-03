5f956d2dd85724cd883d307120140a842a306dda
Jimmy Huang <jimmy@netivism.com.tw>
2013-12-03 20:09:27 +0800

    fixes render timeout too problem.

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ var server = http.createServer(function(req, res){
         }
       }
     });
-    exec("convert -resize 100x -quality 40 "+source+" '"+dest+"_t.jpg'");
+    exec("convert -resize 100x -quality 50 "+source+" '"+dest+"_t.jpg'");
     exec("convert -quality 90 "+source+" '"+dest+"_o.jpg'");
   }
   var p = url.parse(req.url);
--- grab.js
+++ grab.js
@@ -4,15 +4,16 @@ var url = system.args[1];
 var filename = system.args[2];
 page.viewportSize = { width: 1280, height: 1024 };
 page.settings.localToRemoteUrlAccessEnabled = true;
-page.onInitialized = function() {
-  page.evaluate(function() {
-    setTimeout(function() {
-      window.callPhantom();
-    }, 10000);
-  });
-};
-page.open(url, function () {
-  page.clipRect = {top:0, left:0, width:1280, height:1280};
-  page.render(filename);
-  phantom.exit();
+page.open(url, function (status) {
+  page.clipRect = {top:0, left:0, width:1280, height:1024};
+  if (status !== 'success') {
+    page.render(filename);
+    phantom.exit();
+  }
+  else {
+    window.setTimeout(function () {
+      page.render(filename);
+      phantom.exit();
+    }, 15000); // Change timeout as required to allow sufficient time 
+  }
 });
