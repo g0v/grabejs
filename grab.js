69f23d71bfa775918c4d96450edc801fa0c63b57
Jimmy Huang <jimmy@netivism.com.tw>
2013-12-03 19:46:21 +0800

    Remove application.js / add ignore and modify grab config

--- grab.js
+++ grab.js
@@ -2,9 +2,17 @@ var page = require('webpage').create();
 var system = require('system');
 var url = system.args[1];
 var filename = system.args[2];
-page.viewportSize = { width: 1024, height: 768 };
+page.viewportSize = { width: 1280, height: 1024 };
+page.settings.localToRemoteUrlAccessEnabled = true;
+page.onInitialized = function() {
+  page.evaluate(function() {
+    setTimeout(function() {
+      window.callPhantom();
+    }, 10000);
+  });
+};
 page.open(url, function () {
-  page.clipRect = {top:0, left:0, width:1024, height:1024};
+  page.clipRect = {top:0, left:0, width:1280, height:1280};
   page.render(filename);
   phantom.exit();
 });
